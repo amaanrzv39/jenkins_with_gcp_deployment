1326292584ae8786d56a51cd94150abef3b5c6dd
amaanrzv39 <amaan.rizvi39@gmail.com>
2026-06-08 02:19:00 +0530

    jenkins gcr deployment

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def predict():
             return jsonify({"message": "The booking is likely to be cancelled."})
 
 if __name__=="__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0' , port=port)
